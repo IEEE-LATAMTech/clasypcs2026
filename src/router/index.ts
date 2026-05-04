31e7106efc7bb285b05a58d6f90ddbd3cd6bce71
MichMatrix <michel.a.s401@gmail.com>
2026-05-04 03:42:41 -0300

    feat: add index hackathon to  router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AboutUs from '@/views/AboutUs.vue'
+import Hackathon from '@/views/Hackathon.vue'
 
-
-export  const routes =[
-    { path: '/aboutUs' , name:'AboutUs', component:AboutUs}
+export const routes = [
+  { path: '/aboutUs', name: 'AboutUs', component: AboutUs },
+  { path: '/hackathon', name: 'Hackathon', component: Hackathon },
 ]
 
 const router = createRouter({
